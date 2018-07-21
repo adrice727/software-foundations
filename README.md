9405622982d6012dc6c8cc5a2449143d6350faf7
Aaron Rice <adrice727@gmail.com>
2018-07-21 10:55:33 -0700

    Complete Data and Functions.

---
+++ README.md
@@ -0,0 +1,7 @@
+### Exercises for Software Foundations
+
+*The [Software Foundations](https://softwarefoundations.cis.upenn.edu/) series is a broad introduction to the mathematical underpinnings of reliable software.*
+
+*The principal novelty of the series is that every detail is one hundred percent formalized and machine-checked: the entire text of each volume, including the exercises, is literally a "proof script" for the Coq proof assistant.*
+
+*The exposition is intended for a broad range of readers, from advanced undergraduates to PhD students and researchers. No specific background in logic or programming languages is assumed, though a degree of mathematical maturity is helpful. A one-semester course can expect to cover Logical Foundations plus most of Programming Language Foundations or Verified Functional Algorithms, or selections from both.*
---
+++ logical-foundations/Basics.v
@@ -0,0 +1,250 @@
+Module Basics.
+
+(* Enumerated Types *)
+
+Inductive day : Type :=
+  | monday : day
+  | tuesday : day
+  | wednesday : day
+  | thursday : day
+  | friday : day
+  | saturday : day
+  | sunday : day.
+
+Definition next_weekday (d:day) : day :=
+  match d with
+  | monday => tuesday
+  | tuesday => wednesday
+  | wednesday => thursday
+  | thursday => friday
+  | friday => monday
+  | saturday => monday
+  | sunday => monday
+  end.
+
+Example test_next_weekday: (next_weekday friday) = monday.
+Proof. simpl. reflexivity. Qed.
+
+
+(* Booleans *)
+Inductive bool : Type :=
+  | true : bool
+  | false : bool.
+
+
+Definition negb (b:bool) : bool :=
+  match b with
+  | true => false
+  | false => true
+  end.
+Definition andb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => b2
+  | false => false
+  end.
+Definition orb (b1:bool) (b2:bool) : bool :=
+  match b1 with
+  | true => true
+  | false => b2
+  end.
+
+Example test_orb1: (orb true false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_orb2: (orb false false) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_orb3: (orb false true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_orb4: (orb true true) = true.
+Proof. simpl. reflexivity. Qed.
+
+
+Definition nandb (b1:bool) (b2:bool) : bool :=
+  match b1, b2 with
+  | false, false => true
+  | false, _ => true
+  | _, false => true
+  | _ , _ => false
+  end.
+
+Example test_nandb1: (nandb true false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb2: (nandb false false) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb3: (nandb false true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_nandb4: (nandb true true) = false.
+Proof. simpl. reflexivity. Qed.
+
+
+Definition andb3 (b1:bool) (b2:bool) (b3:bool) : bool :=
+  match b1, b2, b3 with 
+  | true, true, true => true
+  | _, _, _ => false
+  end.
+
+Example test_andb31: (andb3 true true true) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_andb32: (andb3 false true true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb33: (andb3 true false true) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_andb34: (andb3 true true false) = false.
+Proof. simpl. reflexivity. Qed.
+
+
+(* Compound Types *)
+
+Inductive rgb : Type :=
+  | red : rgb
+  | green : rgb
+  | blue : rgb.
+Inductive color : Type :=
+  | black : color
+  | white : color
+  | primary : rgb -> color.
+
+Definition monochrome (c : color) : bool :=
+  match c with
+  | black => true
+  | white => true
+  | primary p => false
+  end.
+
+Definition isred (c : color) : bool :=
+  match c with
+  | black => false
+  | white => false
+  | primary red => true
+  | primary _ => false
+  end.
+
+
+Module NatPlayground.
+
+ Inductive nat : Type :=
+  | O : nat
+  | S : nat -> nat.
+
+Inductive nat' : Type :=
+  | stop : nat'
+  | tick : nat' -> nat'.
+
+ Definition pred (n : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => n'
+  end.
+
+End NatPlayground.
+
+Check (S (S (S (S O)))).
+
+Definition minustwo (n : nat) : nat :=
+  match n with
+    | O => O
+    | S O => O
+    | S (S n') => n'
+  end.
+
+Compute (minustwo 4).
+
+Check S.
+Check pred.
+Check minustwo.
+
+Fixpoint evenb (n:nat) : bool :=
+  match n with
+  | O => true
+  | S O => false
+  | S (S n') => evenb n'
+  end.
+
+Definition oddb (n:nat) : bool := negb (evenb n).
+Example test_oddb1: oddb 1 = true.
+Proof. simpl. reflexivity. Qed.
+Example test_oddb2: oddb 4 = false.
+Proof. simpl. reflexivity. Qed.
+
+Module NatPlayground2.
+Fixpoint plus (n : nat) (m : nat) : nat :=
+  match n with
+    | O => m
+    | S n' => S (plus n' m)
+  end.
+
+Compute (plus 3 2).
+
+Fixpoint mult (n m : nat) : nat :=
+  match n with
+    | O => O
+    | S n' => plus m (mult n' m)
+  end.
+
+Example test_mult1: (mult 3 3) = 9.
+Proof. simpl. reflexivity. Qed.
+
+
+Fixpoint minus (n m:nat) : nat :=
+  match n, m with
+  | O , _ => O
+  | S _ , O => n
+  | S n', S m' => minus n' m'
+  end.
+
+End NatPlayground2.
+
+
+Fixpoint factorial (n:nat) : nat :=
+  match n with 
+  | 0 => 1
+  | S n' => mult n (factorial n')
+  end.
+
+Example test_factorial1: (factorial 3) = 6.
+Proof. simpl. reflexivity. Qed.
+Example test_factorial2: (factorial 5) = (mult 10 12).
+Proof. simpl. reflexivity. Qed.
+
+Fixpoint beq_nat (n m : nat) : bool :=
+  match n with
+  | O => match m with
+         | O => true
+         | S m' => false
+         end
+  | S n' => match m with
+            | O => false
+            | S m' => beq_nat n' m'
+            end
+  end.
+
+Fixpoint leb (n m : nat) : bool :=
+  match n with
+  | O => true
+  | S n' =>
+      match m with
+      | O => false
+      | S m' => leb n' m'
+      end
+  end.
+Example test_leb1: (leb 2 2) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb2: (leb 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_leb3: (leb 4 2) = false.
+Proof. simpl. reflexivity. Qed.
+
+Definition blt_nat (n m : nat) : bool :=
+  match beq_nat n m, leb n m with 
+  | false, true => true
+  | _ , _ => false
+  end.
+
+Example test_blt_nat1: (blt_nat 2 2) = false.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat2: (blt_nat 2 4) = true.
+Proof. simpl. reflexivity. Qed.
+Example test_blt_nat3: (blt_nat 4 2) = false.
+Proof. simpl. reflexivity. Qed.
+
+End Basics.
+
